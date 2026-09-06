b08416dd9189dc3af05c8458fd50e79888dafc2c
Click2Find <info.uf.c2f@gmail.com>
2026-09-06 19:42:50 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+
 import Home from "./Home";
+import ProductDetail from "./ProductDetail";
 
 export default function App() {
+  const path = window.location.pathname;
+
+  if (path.includes("/product")) {
+    return <ProductDetail />;
+  }
+
   return <Home />;
 }
