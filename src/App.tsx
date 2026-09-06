1726d776287f34f49b3cbb464430f00b74867a48
Click2Find <info.uf.c2f@gmail.com>
2026-09-06 19:25:25 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
-import ProductDetail from "./ProductDetail";
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/product" element={<ProductDetail />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Home />;
 }
