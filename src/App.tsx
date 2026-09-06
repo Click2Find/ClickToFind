5ea602fb5ddb9ef84fba548e5016ea7562b5b7f5
Click2Find <info.uf.c2f@gmail.com>
2026-09-06 22:41:22 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,29 @@ import { useEffect, useState } from "react";
 import Home from "./Home";
 import ProductDetail from "./ProductDetail";
 import CompanyPage from "./CompanyPage";
+
 export default function App() {
   const [hash, setHash] = useState(window.location.hash);
+
   useEffect(() => {
     const handleHashChange = () => {
       setHash(window.location.hash);
     };
+
     window.addEventListener("hashchange", handleHashChange);
+
     return () => {
       window.removeEventListener("hashchange", handleHashChange);
     };
   }, []);
+
   if (hash === "#product") {
     return <ProductDetail />;
   }
-  if (hash === "#company-nordic-candle-uf") {
+
+  if (hash === "#company") {
     return <CompanyPage />;
   }
+
   return <Home />;
 }
